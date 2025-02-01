f673042a96340ca1ff388a364defcc22872bf205
sanjograiofficial <sanjograiofficial@gmail.com>
2025-02-01 13:29:42 +0545

    Added a bit of styles and content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,43 +2,47 @@ import React from "react";
 
 function About() {
   return (
-    <div>
-      <h1>About Me</h1>
+    <div className="about">
+      <div className="item-1">
+        <h1>About Me</h1>
+        <p>
+          Welcome! I'm Sanjog Rai, an aspiring Web Developer with a deep
+          love for web development, UI/UX design, software
+          engineering. With years of learning experiences, I specialize in creating
+          intuitive user experiences, dynamic web applications, efficient software solutions.
+        </p>
+      </div>
 
-      <p>
-        Welcome! I'm Sanjog Rai, an aspiring Web Developer with a deep
-        love for web development, UI/UX design, software
-        engineering. With years of learning experiences, I specialize in creating
-        intuitive user experiences, dynamic web applications, efficient software solutions.
-      </p>
+      <div className="item-2">
+        <h1>My Journey</h1>
+        <p>
+          My journey in Web Development began when I decided to learn HTML and CSS in my school. Over the years, I have honed my skills in
+          HTML, CSS, JavaScript and React. I have also
+          worked on projects ranging from web applications,
+          websites and some basic projects.
+        </p>
+      </div>
 
-      <h1>My Journey</h1>
+      <div className="item-3">
+        <h1>What Drives Me</h1>
+        <p>
+          I believe in innovation, creativity and efficiency. I am passionate about
+          user-first design, problem-solving. Whether it’s building a sleek,
+          responsive website or optimizing backend performance, I strive to
+          deliver high-quality, impactful solutions that make a difference.
+        </p>
+      </div>
 
-      <p>
-        My journey in Web Development began when I decided to learn HTML and CSS in my school. Over the years, I have honed my skills in
-        HTML, CSS, JavaScript and React. I have also
-        worked on projects ranging from web applications,
-        websites and some basic projects.
-      </p>
+      <div className="item-4">
+        <h1>Beyond Work</h1>
+        <p>
+          When I’m not coding or designing, you’ll find me playing guitar, singing and gaming. I love
+          connecting with like-minded individuals and am always eager to
+          collaborate on exciting projects.
+        </p>
+      </div>
 
-      <h1>What Drives Me</h1>
-
-      <p>
-        I believe in innovation, creativity and efficiency. I am passionate about
-        user-first design, problem-solving. Whether it’s building a sleek,
-        responsive website or optimizing backend performance, I strive to
-        deliver high-quality, impactful solutions that make a difference.
-      </p>
-
-      <h1>Beyond Work</h1>
-
-      <p>
-        When I’m not coding or designing, you’ll find me playing guitar, singing and gaming. I love
-        connecting with like-minded individuals and am always eager to
-        collaborate on exciting projects.
-      </p>
-
-      <h3>Thanks for stopping by! ;{")"}</h3>
+      <h3 className="item-5">Thanks for stopping by! ;{")"}</h3>
     </div>
   );
 }
