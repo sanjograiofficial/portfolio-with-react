03bac41fff08f2934070992b5f9976eac7880b8e
sanjograiofficial <sanjograiofficial@gmail.com>
2025-02-08 18:24:59 +0545

    Added Education

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Education() {
   return (
-    <div>
-      <h1>Education</h1>
+    <div className="edu-container">
+      <div className="card card-1">Grade 10</div>
+      <div className="card card-2">Grade 12</div>
+      <div className="card card-3">Bachelor</div>
     </div>
-  )
+  );
 }
