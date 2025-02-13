d9cd57ef169a6d93510c4467c2c2038a891581d8
sanjograiofficial <sanjograiofficial@gmail.com>
2025-02-13 19:40:08 +0545

    Modified content of Project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import project from "./project";
-import './project.css'
+import "./project.css";
 
 export default function Projects() {
   return (
-    <div>
+    <div className="pro-body">
       <h1>Projects</h1>
       <div className="grid-container">
         {project && project.length > 0
-          ? project.map((item) => <div className="card">
-            <img src={item.imgUrl} alt={item.title} />
-            <h3>{item.title}</h3></div>)
+          ? project.map((item) => (
+              <div className="card">
+                <a href={item.link} target="_blank">
+                  <img src={item.imgUrl} alt={item.title} />
+                  <h3>{item.title}</h3>
+                </a>
+              </div>
+            ))
           : null}
       </div>
     </div>
--- src/pages/project.js
+++ src/pages/project.js
@@ -2,14 +2,30 @@ const project = [
   {
     id: 1,
     title: "Weather App",
-    imgUrl: "",
-    link: "",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZICV0fftz_-7z6RqV753FHwIECEPSxjhi6g&s",
+    link: "https://sanjograiofficial.github.io/weather-app/",
   },
   {
     id: 2,
     title: "Roman Number Conerter",
-    imgUrl: '',
-    link: "",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmQFCHSPHr86OK7V7VitUyo8UfadKIQCtHgQ&s",
+    link: "https://sanjograiofficial.github.io/roman-number-converter/",
+  },
+  {
+    id: 3,
+    title: "Todo",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqR2OaOsYbwmt5Cc9sQZ-uaW_u6LzaqF0Q3A&s",
+    link: "https://sanjograiofficial.github.io/todo-list/",
+  },
+  {
+    id: 4,
+    title: "Palindrome Checker",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQPLDSYn3eqKsuGRCd_LKU0YEEFt-50oVjwg&s",
+    link: "https://sanjograiofficial.github.io/palindrome-checker/",
   },
 ];
 export default project;
